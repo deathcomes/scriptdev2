8619a7239a796560c5f8313e4dd7c29fbd479842
Deathcomes <gangcraft@bk.ru>
2010-01-08 04:27:24 +0700

    -- hack for boss sartharion

--- scripts/northrend/obsidian_sanctum/boss_sartharion.cpp
+++ scripts/northrend/obsidian_sanctum/boss_sartharion.cpp
@@ -50,7 +50,7 @@ enum
     WHISPER_OPEN_PORTAL                         = -1615042, // whisper, shared by two dragons
 
     //Sartharion Spells
-    SPELL_BERSERK                               = 61632,    // Increases the caster's attack speed by 150% and all damage it deals by 500% for 5 min.
+    SPELL_BERSERK                               = 26662,    // Increases the caster's attack speed by 150% and all damage it deals by 500% for 5 min.
     SPELL_CLEAVE                                = 56909,    // Inflicts 35% weapon damage to an enemy and its nearest allies, affecting up to 10 targets.
     SPELL_FLAME_BREATH                          = 56908,    // Inflicts 8750 to 11250 Fire damage to enemies in a cone in front of the caster.
     SPELL_FLAME_BREATH_H                        = 58956,    // Inflicts 10938 to 14062 Fire damage to enemies in a cone in front of the caster.
